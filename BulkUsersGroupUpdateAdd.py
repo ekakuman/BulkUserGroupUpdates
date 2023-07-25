7ea7062bb96a9b5ce4c21317a49221d5928d2000
ekakuman <ephraimkn1976@gmail.com>
2023-07-24 17:10:47 -0700

    Update BulkUsersGroupUpdateAdd.py

--- BulkUsersGroupUpdateAdd.py
+++ BulkUsersGroupUpdateAdd.py
@@ -11,6 +11,7 @@ def get_user_ids(access_token, email_list):
     matched_user_ids = {}
     unmatched_emails = []  # Create a list to store unmatched email addresses
     
+    print("\n######## Begin User ID Retrieval ########\n")
     for email in email_list:
         url = f"{base_url}/people?email={email}"
         response = requests.get(url, headers=headers)
@@ -29,16 +30,15 @@ def get_user_ids(access_token, email_list):
             print(f"User with email '{email}' update has been accepted for processing.")
             matched_user_ids[email] = response_json["id"]
         else:
-            print(f"Failed to retrieve user with email '{email}'. Status code: {response.status_code}")
+            print(f"Failed to retrieve user id with email '{email}'. Status code: {response.status_code}\n")
             unmatched_emails.append(email)
 
     if unmatched_emails:
-        print("Unmatched email addresses:")
-        for email in unmatched_emails:
-            print(email)
-    
-    return matched_user_ids
+        unmatched_emails_count = len(unmatched_emails)
+        print(f"Total unmatched email addresses: {unmatched_emails_count}")
 
+    print("######## End User ID Retrieval ########")
+    return matched_user_ids
 
 def update_group_with_user_ids(access_token, group_id, user_ids):
     base_url = "https://webexapis.com/v1"
@@ -51,21 +51,30 @@ def update_group_with_user_ids(access_token, group_id, user_ids):
     # Create the payload with the updated format
     members = [{"id": user_id, "operation": "add"} for user_id in user_ids.values()]
 
+    successful_emails = []
+    failed_emails = []
+    
+    print("######## Begin Group Membership Update ########\n")
+    print(f"Group ID: {group_id}")
     for count, (email, user_id) in enumerate(user_ids.items(), 1):
         response = requests.patch(url, headers=headers, json={"members": [{"id": user_id, "operation": "add"}]})
-        if response.status_code in (200, 201, 202):
+        items = response.json().get("items")
+        if response.status_code in (200, 201, 204):
             print(f"{count}, user_id: {user_id}, email: {email} - Successfully updated.")
-        elif response.status_code == 204:
-            print(f"{count}, user_id: {user_id}, email: {email} - Successfully updated (No Content).")
+            successful_emails.append(email)
         else:
             print(f"{count}, user_id: {user_id}, email: {email} - Failed to update. Status code: {response.status_code}")
+            failed_emails.append(email)
+    
+    successful_count = len(successful_emails)
+    failed_count = len(failed_emails)
 
-    successful_count = sum(1 for _ in user_ids)
-    failed_count = len(user_ids) - successful_count
+    print(f"Total number of users successfully added to {group_id}: {successful_count}\n")
+    print(f"Total number of users failed to be added to {group_id}: {failed_count}\n")
 
-    print(f"\nGroup {group_id} update summary:")
-    print(f"Successfully updated {successful_count} users.")
-    print(f"Failed to update {failed_count} users.")
+    for email in failed_emails:
+        print(email)
+    print("######## End Group Membership Update ########\n")
 
 def main():
     access_token = input("Enter Access Token: ")
@@ -79,11 +88,10 @@ def main():
         print("CSV Headers:", reader.fieldnames)  # Print the header names to verify
         for row in reader:
             user_emails.append(row["users"])  # Access email addresses using the header "users"
-            print("Read email:", row["users"])  # Print the email being read from CSV
+            #print("Read email:", row["users"])  # Print the email being read from CSV
 
     # Get user IDs
     user_ids = get_user_ids(access_token, user_emails)
-    print(user_ids)
     if user_ids is None:
         return
 
