5b29d905df5c7a0ee73d73eb0ed06152911ff2c2
ekakuman <ephraimkn1976@gmail.com>
2023-07-24 17:11:18 -0700

    Update BulkUsersGroupUpdateRemove.py

--- BulkUsersGroupUpdateRemove.py
+++ BulkUsersGroupUpdateRemove.py
@@ -2,20 +2,42 @@ import requests
 import csv
 import chardet
 
-def get_user_ids(access_token,email_list):
+def get_user_ids(access_token, email_list):
     base_url = "https://webexapis.com/v1"
     headers = {
         "Authorization": f"Bearer {access_token}"
     }
-    url = f"{base_url}/people"
-    response = requests.get(url, headers=headers)
-    response_json = response.json()
-    if response.status_code == 200:
-        user_ids = {user["emails"][0]: user["id"] for user in response_json["items"] if user["emails"][0] in email_list}
-        return user_ids
-    else:
-        print(f"Failed to retrieve user list. Status code: {response.status_code}")
-        return None
+
+    matched_user_ids = {}
+    unmatched_emails = []  # Create a list to store unmatched email addresses
+    
+    print("\n######## Begin User ID Retrieval ########\n")
+    for email in email_list:
+        url = f"{base_url}/people?email={email}"
+        response = requests.get(url, headers=headers)
+        response_json = response.json()
+
+        if response.status_code == 200 and response_json.get("items"):
+            # If there are items in the response, it means the email was matched
+            matched_user_ids[email] = response_json["items"][0]["id"]
+        elif response.status_code == 204:
+            print(f"User with email '{email}' not found.")
+            unmatched_emails.append(email)
+        elif response.status_code == 201:
+            print(f"User with email '{email}' has been created.")
+            matched_user_ids[email] = response_json["id"]
+        elif response.status_code == 202:
+            print(f"User with email '{email}' update has been accepted for processing.")
+            matched_user_ids[email] = response_json["id"]
+        else:
+            print(f"Failed to retrieve user with email '{email}'. Status code: {response.status_code}")
+            unmatched_emails.append(email)
+
+    if unmatched_emails:
+        print("Unmatched email addresses:")
+    
+    print("######## End User ID Retrieval ########")
+    return matched_user_ids
 
 def update_group_with_user_ids(access_token, group_id, user_ids):
     base_url = "https://webexapis.com/v1"
@@ -26,24 +48,32 @@ def update_group_with_user_ids(access_token, group_id, user_ids):
     url = f"{base_url}/groups/{group_id}"
     
     # Create the payload with the updated format
-    members = [{"id": user_id, "operation": "delete"} for user_id in user_ids.values()]
+    members = [{"id": user_id, "operation": "add"} for user_id in user_ids.values()]
+
+    successful_emails = []
+    failed_emails = []
     
+    print("######## Begin Group Membership Update ########\n")
+    print(f"Group ID: {group_id}")
     for count, (email, user_id) in enumerate(user_ids.items(), 1):
         response = requests.patch(url, headers=headers, json={"members": [{"id": user_id, "operation": "delete"}]})
-    if response.status_code in (200, 201, 202):
-        print(f"{count}, user_id: {user_id}, email: {email} - Successfully removed.")
-    elif response.status_code == 204:
-        print(f"{count}, user_id: {user_id}, email: {email} - Successfully updated (No Content).")
-    else:
-        print(f"{count}, user_id: {user_id}, email: {email} - Failed to update. Status code: {response.status_code}")
+        items = response.json().get("items")
+        if response.status_code in (200, 201, 204):
+            print(f"{count}, user_id: {user_id}, email: {email} - Successfully removed.")
+            successful_emails.append(email)
+        else:
+            print(f"{count}, user_id: {user_id}, email: {email} - Failed to update. Status code: {response.status_code}")
+            failed_emails.append(email)
+    
+    successful_count = len(successful_emails)
+    failed_count = len(failed_emails)
 
-    successful_count = sum(1 for _ in user_ids)
-    failed_count = len(user_ids) - successful_count
-
-    print(f"\nGroup {group_id} update summary:")
-    print(f"Successfully removed {successful_count} users.")
-    print(f"Failed to remove {failed_count} users.")
-
+    print(f"Total number of users successfully removed from {group_id}: {successful_count}\n")
+    print(f"Total number of users failed to be removed from {group_id}: {failed_count}\n")
+    
+    for email in failed_emails:
+        print(email)
+    print("######## End Group Membership Update ########\n")
 
 def main():
     access_token = input("Enter Access Token: ")
@@ -52,14 +82,16 @@ def main():
 
     # Read user emails from the CSV file
     user_emails = []
-    with open(csv_file, "r",encoding="utf-8-sig") as csvfile:
+    with open(csv_file, "r", encoding="utf-8-sig") as csvfile:
         reader = csv.DictReader(csvfile)
         print("CSV Headers:", reader.fieldnames)  # Print the header names to verify
         for row in reader:
             user_emails.append(row["users"])  # Access email addresses using the header "users"
-            
+            print("Read email:", row["users"])  # Print the email being read from CSV
+
     # Get user IDs
-    user_ids = get_user_ids(access_token,user_emails)
+    user_ids = get_user_ids(access_token, user_emails)
+    print(user_ids)
     if user_ids is None:
         return
 
