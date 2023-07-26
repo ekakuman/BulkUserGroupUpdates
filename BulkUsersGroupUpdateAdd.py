e494a65989c23b3bb11e4471db15b2c2318a3683
ekakuman <ephraimkn1976@gmail.com>
2023-07-26 16:42:21 -0700

    Update BulkUsersGroupUpdateAdd.py

--- BulkUsersGroupUpdateAdd.py
+++ BulkUsersGroupUpdateAdd.py
@@ -13,6 +13,8 @@ def get_user_ids(access_token, email_list):
     unmatched_emails = []  # Create a list to store unmatched email addresses
 
     print("\n######## Begin User ID Retrieval ########\n")
+    processed_count = 0
+    total_emails = len(email_list)
     for email in email_list:
         url = f"{base_url}/people?email={email}"
         response = requests.get(url, headers=headers)
@@ -31,7 +33,7 @@ def get_user_ids(access_token, email_list):
             print(f"User with email '{email}' update has been accepted for processing.")
             matched_user_ids[email] = response_json["id"]
         elif response.status_code == 429:
-            wait_time = int(response.headers["Retry-After"])
+            wait_time = int(response.headers.get("Retry-After", "10"))  # Default to 10 seconds if Retry-After header is not present
             print(f"Rate limit exceeded. Waiting for {wait_time} seconds...")
             start_time = time.time()
             time.sleep(wait_time)
@@ -49,14 +51,22 @@ def get_user_ids(access_token, email_list):
             print(f"Failed to retrieve user id with email '{email}'. Status code: {response.status_code}\n")
             unmatched_emails.append(email)
 
+        processed_count += 1
+        if processed_count % 10 == 0:  # Print an update after every 10 emails processed
+            print(f"Processed {processed_count}/{total_emails} users")
+
         # Add a delay of 0.2 seconds between requests to avoid reaching rate limits too quickly
         time.sleep(0.2)
 
+    if matched_user_ids:
+        matched_emails_count = len(matched_user_ids)
+        print(f"\nTotal matched email addresses: {matched_emails_count}")
+
     if unmatched_emails:
         unmatched_emails_count = len(unmatched_emails)
-        print(f"Total unmatched email addresses: {unmatched_emails_count}")
+        print(f"\nTotal unmatched email addresses: {unmatched_emails_count}")
 
-    print("######## End User ID Retrieval ########")
+    print("######## End User ID Retrieval ########\n")
     return matched_user_ids
 
 def update_group_with_user_ids(access_token, group_id, user_ids):
@@ -76,23 +86,23 @@ def update_group_with_user_ids(access_token, group_id, user_ids):
     print("######## Begin Group Membership Update ########\n")
     print(f"Group ID: {group_id}")
     for count, (email, user_id) in enumerate(user_ids.items(), 1):
-        response = requests.patch(url, headers=headers, json={"members": [{"id": user_id, "operation": "add"}]})
+        response = requests.patch(url, headers=headers, json={"members": [{"id": user_id, "operation": "delete"}]})
         items = response.json().get("members")
         if response.status_code in (200, 201, 204):
-            if response.status_code == 200 and items:
-                print(f"{count}, user_id: {user_id}, email: {email} - Successfully updated.")
+            if response.status_code == 200:
+                print(f"{count}, user_id: {user_id}, email: {email} - Successfully removed.")
                 successful_emails.append(email)
             elif response.status_code == 201:
-                print(f"{count}, user_id: {user_id}, email: {email} - User has been created.")
+                print(f"{count}, user_id: {user_id}, email: {email} - User has been removed.")
                 successful_emails.append(email)
             elif response.status_code == 202:
-                print(f"{count}, user_id: {user_id}, email: {email} - Update has been accepted for processing.")
+                print(f"{count}, user_id: {user_id}, email: {email} - Remove Request has been accepted for processing.")
                 successful_emails.append(email)
             elif response.status_code == 204:
-                print(f"{count}, user_id: {user_id}, email: {email} - Successful request without body content.")
+                print(f"{count}, user_id: {user_id}, email: {email} - Successful remove request without body content.")
                 successful_emails.append(email)
             else:
-                print(f"{count}, user_id: {user_id}, email: {email} - Failed to update. Empty response.\n")
+                print(f"{count}, user_id: {user_id}, email: {email} - Failed to remove. Empty response.\n")
                 failed_emails.append(email)
         elif response.status_code == 429:
             wait_time = int(response.headers["Retry-After"])
@@ -105,13 +115,13 @@ def update_group_with_user_ids(access_token, group_id, user_ids):
             response = requests.patch(url, headers=headers, json={"members": [{"id": user_id, "operation": "add"}]})
             items = response.json().get("items")
             if response.status_code in (200, 201, 204) and items:
-                print(f"{count}, user_id: {user_id}, email: {email} - Successfully updated.")
+                print(f"{count}, user_id: {user_id}, email: {email} - Successfully removed.")
                 successful_emails.append(email)
             else:
-                print(f"{count}, user_id: {user_id}, email: {email} - Failed to update even after retry. Status code: {response.status_code}\n")
+                print(f"{count}, user_id: {user_id}, email: {email} - Failed to remove even after retry. Status code: {response.status_code}\n")
                 failed_emails.append(email)
         else:
-            print(f"{count}, user_id: {user_id}, email: {email} - Failed to update. Status code: {response.status_code}\n")
+            print(f"{count}, user_id: {user_id}, email: {email} - Failed to remove. Status code: {response.status_code}\n")
             failed_emails.append(email)
 
         # Add a delay of 0.2 seconds between requests to avoid reaching rate limits too quickly
@@ -120,8 +130,8 @@ def update_group_with_user_ids(access_token, group_id, user_ids):
     successful_count = len(successful_emails)
     failed_count = len(failed_emails)
 
-    print(f"\nTotal number of users successfully added to {group_id}: {successful_count}\n")
-    print(f"Total number of users failed to be added to {group_id}: {failed_count}\n")
+    print(f"\nTotal number of users successfully removed from {group_id}: {successful_count}\n")
+    print(f"Total number of users failed to be removed from {group_id}: {failed_count}\n")
 
     for email in failed_emails:
         print(email)
@@ -140,7 +150,7 @@ def main():
         print("CSV Headers:", reader.fieldnames)  # Print the header names to verify
         for row in reader:
             user_emails.append(row["users"])  # Access email addresses using the header "users"
-            #print("Read email:", row["users"])  # Print the email being read from CSV
+            print("Read email:", row["users"])  # Print the email being read from CSV
 
     # Get user IDs
     user_ids = get_user_ids(access_token, user_emails)
