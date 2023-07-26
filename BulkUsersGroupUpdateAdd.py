d944e260ded0de26a2f95d1bc26c3969e341525b
ekakuman <ephraimkn1976@gmail.com>
2023-07-26 11:23:08 -0700

    Update BulkUsersGroupUpdateAdd.py

--- BulkUsersGroupUpdateAdd.py
+++ BulkUsersGroupUpdateAdd.py
@@ -1,6 +1,7 @@
 import requests
 import csv
 import chardet
+import time
 
 def get_user_ids(access_token, email_list):
     base_url = "https://webexapis.com/v1"
@@ -10,7 +11,7 @@ def get_user_ids(access_token, email_list):
 
     matched_user_ids = {}
     unmatched_emails = []  # Create a list to store unmatched email addresses
-    
+
     print("\n######## Begin User ID Retrieval ########\n")
     for email in email_list:
         url = f"{base_url}/people?email={email}"
@@ -29,9 +30,27 @@ def get_user_ids(access_token, email_list):
         elif response.status_code == 202:
             print(f"User with email '{email}' update has been accepted for processing.")
             matched_user_ids[email] = response_json["id"]
+        elif response.status_code == 429:
+            wait_time = int(response.headers["Retry-After"])
+            print(f"Rate limit exceeded. Waiting for {wait_time} seconds...")
+            start_time = time.time()
+            time.sleep(wait_time)
+            end_time = time.time()
+            print(f"Waited for {end_time - start_time} seconds before retrying.")
+            # Retry the request after waiting for the specified seconds
+            response = requests.get(url, headers=headers)
+            response_json = response.json()
+            if response.status_code == 200 and response_json.get("items"):
+                matched_user_ids[email] = response_json["items"][0]["id"]
+            else:
+                print(f"Failed to retrieve user id with email '{email}' even after retry. Status code: {response.status_code}\n")
+                unmatched_emails.append(email)
         else:
             print(f"Failed to retrieve user id with email '{email}'. Status code: {response.status_code}\n")
             unmatched_emails.append(email)
+
+        # Add a delay of 0.2 seconds between requests to avoid reaching rate limits too quickly
+        time.sleep(0.2)
 
     if unmatched_emails:
         unmatched_emails_count = len(unmatched_emails)
@@ -47,33 +66,66 @@ def update_group_with_user_ids(access_token, group_id, user_ids):
         "Content-Type": "application/json"
     }
     url = f"{base_url}/groups/{group_id}"
-    
+
     # Create the payload with the updated format
     members = [{"id": user_id, "operation": "add"} for user_id in user_ids.values()]
 
     successful_emails = []
     failed_emails = []
-    
+
     print("######## Begin Group Membership Update ########\n")
     print(f"Group ID: {group_id}")
     for count, (email, user_id) in enumerate(user_ids.items(), 1):
         response = requests.patch(url, headers=headers, json={"members": [{"id": user_id, "operation": "add"}]})
-        items = response.json().get("items")
+        items = response.json().get("members")
         if response.status_code in (200, 201, 204):
-            print(f"{count}, user_id: {user_id}, email: {email} - Successfully updated.")
-            successful_emails.append(email)
+            if response.status_code == 200 and items:
+                print(f"{count}, user_id: {user_id}, email: {email} - Successfully updated.")
+                successful_emails.append(email)
+            elif response.status_code == 201:
+                print(f"{count}, user_id: {user_id}, email: {email} - User has been created.")
+                successful_emails.append(email)
+            elif response.status_code == 202:
+                print(f"{count}, user_id: {user_id}, email: {email} - Update has been accepted for processing.")
+                successful_emails.append(email)
+            elif response.status_code == 204:
+                print(f"{count}, user_id: {user_id}, email: {email} - Successful request without body content.")
+                successful_emails.append(email)
+            else:
+                print(f"{count}, user_id: {user_id}, email: {email} - Failed to update. Empty response.\n")
+                failed_emails.append(email)
+        elif response.status_code == 429:
+            wait_time = int(response.headers["Retry-After"])
+            print(f"Rate limit exceeded. Waiting for {wait_time} seconds...")
+            start_time = time.time()
+            time.sleep(wait_time)
+            end_time = time.time()
+            print(f"Waited for {end_time - start_time} seconds before retrying.")
+            # Retry the request after waiting for the specified seconds
+            response = requests.patch(url, headers=headers, json={"members": [{"id": user_id, "operation": "add"}]})
+            items = response.json().get("items")
+            if response.status_code in (200, 201, 204) and items:
+                print(f"{count}, user_id: {user_id}, email: {email} - Successfully updated.")
+                successful_emails.append(email)
+            else:
+                print(f"{count}, user_id: {user_id}, email: {email} - Failed to update even after retry. Status code: {response.status_code}\n")
+                failed_emails.append(email)
         else:
-            print(f"{count}, user_id: {user_id}, email: {email} - Failed to update. Status code: {response.status_code}")
+            print(f"{count}, user_id: {user_id}, email: {email} - Failed to update. Status code: {response.status_code}\n")
             failed_emails.append(email)
-    
+
+        # Add a delay of 0.2 seconds between requests to avoid reaching rate limits too quickly
+        time.sleep(0.2)
+
     successful_count = len(successful_emails)
     failed_count = len(failed_emails)
 
-    print(f"Total number of users successfully added to {group_id}: {successful_count}\n")
+    print(f"\nTotal number of users successfully added to {group_id}: {successful_count}\n")
     print(f"Total number of users failed to be added to {group_id}: {failed_count}\n")
 
     for email in failed_emails:
         print(email)
+
     print("######## End Group Membership Update ########\n")
 
 def main():
